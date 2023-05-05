be462756ddbbd36a67b77f32cb7a678e9a351093
Jay Mange <roryotoole28@gmail.com>
2023-05-05 16:22:54 +0000

    add speed of snake to game.js

--- game.js
+++ game.js
@@ -1,11 +1,15 @@
 let lastRenderTime = 0
+const SNAKE_SPEED = 2
 
 /**Game loop that tells browser when to render next frame */
 function main(currentTime) {
-    const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
     window.requestAnimationFrame(main)
+    const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    console.log('Render')
     lastRenderTime = currentTime
-    console.log(secondSinceLastRender)
 }
 
 window.requestAnimationFrame(main)
