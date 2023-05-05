117b69994edd31aeb703c641c1c16c0ae9323368
Jay Mange <roryotoole28@gmail.com>
2023-05-05 16:48:52 +0000

    add starting location of snake to snake.js

--- snake.js
+++ snake.js
@@ -1,9 +1,18 @@
+/* Code for the Snakes body **/
+
 export const SNAKE_SPEED = 1
+const snakeBody =[{ x: 11, y: 11 }]
 
 export function update() {
     console.log('update snake')
 }
 
+/** Start position of snake */
 export function draw() {
-    console.log('draw snake')
-}
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.x
+        snakeElement.style.gridColumnStart = segment.y
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
+    })
