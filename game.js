e15c576f69f9a62fc94fcb000dda98cb5a31c9f9
Jay Mange <roryotoole28@gmail.com>
2023-05-05 16:18:42 +0000

    add code to detect last render

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
-let lastRenderTime = 0;
+let lastRenderTime = 0
 
+/**Game loop that tells browser when to render next frame */
 function main(currentTime) {
-    window.requestAnimationFrame(main);
-    lastRenderTime = currentTime;
-    console.log(currentTime);
+    const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
+    window.requestAnimationFrame(main)
+    lastRenderTime = currentTime
+    console.log(secondSinceLastRender)
 }
 
 window.requestAnimationFrame(main)
