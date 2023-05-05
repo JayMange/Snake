b65114082124d12cea34065add62cb0b02241184
Jay Mange <roryotoole28@gmail.com>
2023-05-05 22:30:48 +0000

    add check death framework to game.js

--- game.js
+++ game.js
@@ -3,10 +3,15 @@ import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.j
 import { update as updateFood, draw as drawFood } from "./food.js"
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 /**Game loop that tells browser when to render next frame */
 function main(currentTime) {
+    if (gameOver) {
+       return alert('You Lose')
+    }
+
     window.requestAnimationFrame(main)
     const secondSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondSinceLastRender < 1 / SNAKE_SPEED) return
@@ -25,6 +30,7 @@ window.requestAnimationFrame(main)
 function update() {
     updateSnake()
     updateFood()
+    checkDeath()
 }
 
 /**This displays the gameboard and changes made to it */
@@ -32,4 +38,9 @@ function draw() {
     gameBoard.innerHTML = ''
     drawSnake(gameBoard)
     drawFood(gameBoard)
+}
+
+/**This tracks the players deaths */
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersect()
 }
